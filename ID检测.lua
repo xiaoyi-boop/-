1799e54be16120e7e3ffed8cd074f72c63b356b5
xiaoyi-boop <1701291983@qq.com>
2025-03-09 21:45:50 +0800

    Add files via upload

---
+++ ID检测.lua
@@ -0,0 +1,43 @@
+if game.PlaceId == 6839171747 then--doors
+    LENGScript = "名脚本"
+--脚本链接🔗
+elseif game.PlaceId == 3623096087 then--力量传奇
+    LENGScript = "名脚本"
+--脚本链接
+elseif game.PlaceId == 7239319209 then--Ohio
+    LENGScript = "名脚本"
+--脚本链接
+elseif game.PlaceId == 4618049391 then--河北唐县
+    LENGScript = "名脚本"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoyi-boop/-/refs/heads/main/%E6%B2%B3%E5%8C%97%E5%94%90%E5%8E%BF.lua",true))()
+elseif game.PlaceId == 18241512421 then-- 狗熊岭危机
+    LENGScript = "名脚本"
+--脚本链接🔗
+elseif game.PlaceId == 5256165620 then-- 火箭发射模拟器
+    LENGScript = "名脚本"
+--脚本链接🔗
+elseif game.PlaceId == 12931609417 then-- 颜色或死亡
+    LENGScript = "名脚本"
+--脚本链接🔗
+elseif game.PlaceId == 110333320616502 then-- 格蕾丝
+    LENGScript = "名脚本"
+--脚本链接
+elseif game.PlaceId == 138837502355157 then-- 格蕾丝
+    LENGScript = "名脚本"
+--脚本链接
+elseif game.PlaceId == 13822889 then-- 伐木大亨2
+    LENGScript = "名脚本"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wumingjiaoben/ui/refs/heads/main/%E4%BC%90%E6%9C%A8%E5%A4%A7%E4%BA%A8(2).lua"))()
+elseif game.PlaceId == 12196278347 then--Refinery Caves
+    LENGScript = "名脚本"
+--脚本链接
+elseif game.PlaceId == 2753915549 then--BF
+    LENGScript = "名脚本"
+--脚本链接🔗
+elseif game.PlaceId == 3101667897 then--极速传奇
+    LENGScript = "名脚本"
+--脚本链接🔗
+else--通用
+    LENGScript = "名脚本"
+--脚本链接
+end
