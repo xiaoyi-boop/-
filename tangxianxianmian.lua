ee594d46d6cde991f60bad5583ae0cddec2269e6
xiaoyi-boop <1701291983@qq.com>
2025-06-16 15:52:26 +0800

    Add files via upload

---
+++ tangxianxianmian.lua
@@ -0,0 +1,5 @@
+if TANGXIAN == "卡车刷钱限时免费版" then
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoyi-boop/-/refs/heads/main/789.lua",true))()
+else
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoyi-boop/-/refs/heads/main/%E4%BE%9D.lua",true))()
+end
